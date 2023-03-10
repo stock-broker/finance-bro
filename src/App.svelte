d0db95ea989697c3bcbf63f55d19a4a30039bd0d
Marcello San Agustin <marcello@Marcellos-MacBook-Pro.local>
2023-03-09 20:53:33 -0800

    adding

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let income;
+</script>
+
+<main>
+	<div class="grid justify-items-stretch">
+		<h1 class="sm pt-6 rounded-full justify-self-auto text-center text-lg">
+			Enter your Monthly Income
+		</h1>
+
+		<form class=" pl-10 border-solid border-black shadow justify-self-auto">
+			<input
+				class="justify-self-auto"
+				type="text"
+				bind:value={income}
+			/>
+		</form>
+	</div>
+</main>
+
+<style>
+</style>
